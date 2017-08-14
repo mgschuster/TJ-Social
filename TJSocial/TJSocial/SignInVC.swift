c82daa48465817c538dcfeb70ba1fd3beadaf70f
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-14 19:34:00 +0300

    Facebook Authentication

--- TJSocial/TJSocial/SignInVC.swift
+++ TJSocial/TJSocial/SignInVC.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,33 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("MITCHELL Unable to authenticate with Facebook - \(error!)")
+            } else if result?.isCancelled == true {
+                print("MITCHELL User cancelled Facebook authentication")
+            } else {
+                print("MITCHELL Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("MITCHELL Unable to authenticate with Firebase = \(error!)")
+            } else {
+                print("MITCHELL Successfully authenticated with Firebase")
+            }
+        }
+    }
 
 }
 
